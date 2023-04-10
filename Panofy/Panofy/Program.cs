fe1740a1ffc69ae4106ec50c71485cb590e716da
Farhad Zamiri <farhad.zamiri@gmail.com>
2023-04-10 12:35:14 +0200

    Add message to Main method in Program class

--- Panofy/Panofy/Program.cs
+++ Panofy/Panofy/Program.cs
@@ -2,5 +2,6 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, we are developing a sofware");
+        Console.WriteLine("The software consists of so many parts");
     }
 }
