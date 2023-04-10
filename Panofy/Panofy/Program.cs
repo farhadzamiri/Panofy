9550f4c3ee0a1ca95e7dc6aef3d48d327b409e7a
Farhad Zamiri <farhad.zamiri@gmail.com>
2023-04-10 12:34:00 +0200

    Change Program class

--- Panofy/Panofy/Program.cs
+++ Panofy/Panofy/Program.cs
@@ -1,6 +1,6 @@
 ﻿public partial class Program {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, we are developing a sofware");
     }
 }
